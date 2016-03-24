ee1948206395b899d63985b36ca150378f9ab0cb
Trevor Overman <troverman@gmail.com>
2016-03-24 13:45:52 -0400

    remove production db for now

--- config/connections.js
+++ config/connections.js
@@ -21,9 +21,7 @@
 module.exports.connections = {
 
   productionMongoHqDb: {
-    adapter: 'sails-mongo',
-    url: 'mongodb://heroku_xwbztwmv:j7i1brsvpfsph83212vvngi673@ds039431.mongolab.com:39431/heroku_xwbztwmv'
-    //url: process.env.MONGOLAB_URI
+    adapter: 'sails-local',
   }
 
 };
