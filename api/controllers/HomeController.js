9121c8ecc5d83e43c2950519898e296ed421d1f8
Trevor Overman <troverman@gmail.com>
2017-03-27 02:34:37 -0400

    ssl

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -4,5 +4,10 @@ module.exports = {
             title: 'Home',
             currentUser: req.user
         });
-    }
+    },
+
+    ssl: function(req, res) {
+        res.send('Dtkozjq3bbAFbmG2JYv2KB9RFoUIzyBiVd3Zxo0l7aM.yMz-EAV5agQah1zn-w6Aqp0JVzxv1jmSFH6dh5Ea9uI')
+    },
+    
 };
